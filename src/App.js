15bd1c67c969fabd8c7410194c25b2864f9a4bea
Evan Moon <bboydart91@gmail.com>
2019-12-01 15:37:07 +0900

    FontAwesome 설치

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import 'src/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import Editor from 'src/components/Editor';
 
 function App () {
