542362267107f7264fe22bc559f060e3d3bc80dc
Evan Moon <bboydart91@gmail.com>
2019-12-06 15:57:05 +0900

    WaveFormColors를 Hex 포맷으로 변경

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const AsideDefaultWidth = 300;
-export const WaveFormColors = ['darkturquoise', 'blueviolet ', 'brown', 'coral', 'crimson', 'darkblue', 'darkgreen'];
+export const AsideDefaultWidth = 250;
+export const WaveFormColors = ['#00CED1', '#8A2BE2 ', '#A52A2A', '#FF6347', '#00008B', '#DC143C', '#006400'];
